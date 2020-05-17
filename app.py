f9eb46154413e6f1d728a1fe9afe304811cb9474
krisswiss <chrisswiatek@hotmail.com>
2020-05-17 15:30:54 +0000

    added tasks.html

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task-manager-flask-mongo-12'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-8xuvz.mongodb.net/task-manager-flask-mongo-12?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 
 if __name__ == '__main__':
